75ad5a87261be980e8d172ba961929464a069f9e
Gretar Atli Gretarsson <gretaratlig09@gmail.com>
2022-08-04 13:49:41 +0200

    initial hello world project setup

---
+++ Hello_World/activate_environment.sh
@@ -0,0 +1,1 @@
+conda activate kedro-environment
---
+++ Hello_World/hello_kedro.py
@@ -0,0 +1,34 @@
+"""Contents of hello_kedro.py"""
+from kedro.io import DataCatalog, MemoryDataSet
+from kedro.pipeline import node, pipeline
+from kedro.runner import SequentialRunner
+
+# Prepare a data catalog
+data_catalog = DataCatalog({"my_salutation": MemoryDataSet()})
+
+# Prepare first node
+def return_greeting():
+    return "Hello"
+
+# Prepare second node
+def join_statements(greeting):
+    return f"{greeting} Kedro!"
+
+return_greeting_node = node(return_greeting,
+                            inputs=None,
+                            outputs="my_salutation")
+
+join_statements_node = node(join_statements,
+                            inputs="my_salutation",
+                            outputs="my_message")
+
+# Assemble nodes into a pipeline
+greeting_pipeline = pipeline([return_greeting_node,
+                              join_statements_node])
+
+# Create a runner to run the pipeline
+runner = SequentialRunner()
+
+# Run the pipeline
+print(runner.run(greeting_pipeline,
+                 data_catalog))
